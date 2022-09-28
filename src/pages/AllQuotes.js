7ccbc10dc6bc4c3702100b8b7f3d265b52c901de
Vincent Kipngetich <vinniebluz@icloud.com>
2022-09-28 16:09:50 +0300

    Added Dummy Data and more content

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,5 +1,17 @@
+import { Fragment } from "react";
+import QuoteList from "../components/quotes/QuoteList";
+
+const DUMMY_QUOTES = [
+  { id: "q1", author: "Max", text: "Learning React is fun!" },
+  { id: "q2", author: "Vincent", text: "Learning React is great!" },
+];
+
 const AllQuotes = () => {
-  return <h1>All Quotes Page</h1>;
+  return (
+    <Fragment>
+      <QuoteList quotes = {DUMMY_QUOTES}/>
+    </Fragment>
+  );
 };
 
 export default AllQuotes;
